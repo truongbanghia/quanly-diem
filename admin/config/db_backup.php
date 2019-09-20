01ca99078af6fb22a38c147313ed5e1c48189e93
truongbanghia <truongbanghia@gmail.com>
2019-09-20 18:12:09 +0700

    backup db

--- admin/config/db_backup.php
+++ admin/config/db_backup.php
@@ -1,85 +1,48 @@
 <?php
     //ENTER THE RELEVANT INFO BELOW
-    $mysqlUserName      = "root";
-    $mysqlPassword      = "";
-    $mysqlHostName      = "localhost";
-    $DbName             = "quanlydiem";
-    $backup_name        = "db_bk_ql_diem.sql";
-    $tables             = array("dayhoc", "diem", "giaovien", "hocky", "hocsinh", "lophoc", "monhoc","user");
+    include_once('connect.php');
 
-   //or add 5th parameter(array) of specific tables:    array("mytable1","mytable2","mytable3") for multiple tables
+    $backup_name        = 'mybackup.sql';
+    $tables             = array('dayhoc', 'diem', 'giaovien', 'hocky', 'hocsinh', 'lophoc', 'monhoc', 'user');
+    
 
-    Export_Database($mysqlHostName,$mysqlUserName,$mysqlPassword,$DbName,  $tables=false, $backup_name=false );
-
-    function Export_Database($host,$user,$pass,$name,  $tables=false, $backup_name=false )
-    {
-        $mysqli = new mysqli($host,$user,$pass,$name);
-        $mysqli->select_db($name);
-        $mysqli->query("SET NAMES 'utf8'");
-
-        $queryTables    = $mysqli->query('SHOW TABLES');
-        while($row = $queryTables->fetch_row())
-        {
-            $target_tables[] = $row[0];
-        }
-        if($tables !== false)
-        {
-            $target_tables = array_intersect( $target_tables, $tables);
-        }
-        foreach($target_tables as $table)
-        {
-            $result         =   $mysqli->query('SELECT * FROM '.$table);
-            $fields_amount  =   $result->field_count;
-            $rows_num=$mysqli->affected_rows;
-            $res            =   $mysqli->query('SHOW CREATE TABLE '.$table);
-            $TableMLine     =   $res->fetch_row();
-            $content        = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
-
-            for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0)
-            {
-                while($row = $result->fetch_row())
-                { //when started (and every after 100 command cycle):
-                    if ($st_counter%100 == 0 || $st_counter == 0 )
-                    {
-                            $content .= "\nINSERT INTO ".$table." VALUES";
-                    }
-                    $content .= "\n(";
-                    for($j=0; $j<$fields_amount; $j++)
-                    {
-                        $row[$j] = str_replace("\n","\\n", addslashes($row[$j]) );
-                        if (isset($row[$j]))
-                        {
-                            $content .= '"'.$row[$j].'"' ;
-                        }
-                        else
-                        {
-                            $content .= '""';
-                        }
-                        if ($j<($fields_amount-1))
-                        {
-                                $content.= ',';
-                        }
-                    }
-                    $content .=")";
-                    //every after 100 command cycle [or at last line] ....p.s. but should be inserted 1 cycle eariler
-                    if ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num)
-                    {
-                        $content .= ";";
-                    }
-                    else
-                    {
-                        $content .= ",";
-                    }
-                    $st_counter=$st_counter+1;
-                }
-            } $content .="\n\n\n";
-        }
-        //$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
-        $date = date("Y-m-d");
-        $backup_name = $backup_name ? $backup_name : $name.".$date.sql";
-        header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=\"".$backup_name."\"");
-        echo $content; exit;
+    $result = mysqli_query($conn,"SHOW TABLES");
+    while ($row = mysqli_fetch_row($result)) {
+        $tables[] = $row[0];
     }
-?>
+    
+    $return = '';
+    
+    foreach ($tables as $table) {
+        $result = mysqli_query($conn, "SELECT * FROM ".$table);
+        $num_fields = mysqli_num_fields($result);
+    
+        $return .= 'DROP TABLE '.$table.';';
+        $row2 = mysqli_fetch_row(mysqli_query($conn, 'SHOW CREATE TABLE '.$table));
+        $return .= "\n\n".$row2[1].";\n\n";
+    
+        for ($i=0; $i < $num_fields; $i++) { 
+            while ($row = mysqli_fetch_row($result)) {
+                $return .= 'INSERT INTO '.$table.'VALUES(';
+                for ($j=0; $j < $num_fields; $j++) { 
+                    $row[$j] = addslashes($row[$j]);
+                    if (isset($row[$j])) {
+                        $return .= '"'.$row[$j].'"';} else { $return .= '""';}
+                        if($j<$num_fields-1){ $return .= ','; }
+                    }
+                    $return .= ");\n";
+                }
+            }
+            $return .= "\n\n\n";
+        
+    }
+    
+    
+    $handle = fopen('backup_db_qldiem.sql', 'w+');
+    fwrite($handle, $return);
+    fclose($handle);
+    echo "success";
+    
+    
+    ?>
+    
